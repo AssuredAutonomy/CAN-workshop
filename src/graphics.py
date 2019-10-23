c2d4694cc17f615c6f8ed1188268c5915441510f
Chris Liu <liu.6948@osu.edu>
2019-10-23 16:39:42 -0400

    Spawned steering wheel and modified IPC image in GUI

--- src/graphics.py
+++ src/graphics.py
@@ -4,14 +4,17 @@ import sdl2.ext
 class Gui():
     def __init__(self):
         sdl2.ext.init()
-        self.window = sdl2.ext.Window("IPC", size=(1000, 500))
+        self.window = sdl2.ext.Window("IPC", size=(1000, 600))
         self.window.show()
         self.world = sdl2.ext.World()
-        self.renderer =  sdl2.ext.Renderer(self.window)
+        self.renderer = sdl2.ext.Renderer(self.window)
         self.factory = sdl2.ext.SpriteFactory(sdl2.ext.TEXTURE, renderer=self.renderer)
-        self.ipc = self.factory.from_image("img/IPC.png")
+        self.ipc = self.factory.from_image("img/IPC_2.png")
         self.tac_needle = self.factory.from_image("img/Pointer.png")
         self.speed_needle = self.factory.from_image("img/Pointer.png")
+        self.blinker_L = self.factory.from_image("img/Blinker_L.png")
+        self.blinker_R = self.factory.from_image("img/Blinker_R.png")
+        self.steering_wheel = self.factory.from_image("img/steering_wheel.png")
 
         def set_gui():
             self.tac_needle.x = 115
@@ -20,14 +23,21 @@ class Gui():
             self.speed_needle.y = 78
             self.tac_needle.angle = 160
             self.speed_needle.angle = 160
+            self.steering_wheel.x = 400
+            self.steering_wheel.y = 350
+            self.steering_wheel.angle = 0
             self.spriterenderer = self.factory.create_sprite_render_system(self.window)
             self.world.add_system(self.spriterenderer)
             self.tec_ent = needleEntity(self.world, self.tac_needle, self.tac_needle.angle)
+
 
         def display_gui():
             self.spriterenderer.render(self.ipc)
             self.spriterenderer.render(self.tac_needle)
             self.spriterenderer.render(self.speed_needle)
+            self.spriterenderer.render(self.blinker_L)
+            self.spriterenderer.render(self.blinker_R)
+            self.spriterenderer.render(self.steering_wheel)
             #processor = sdl2.ext.TestEventProcessor()
             #processor.run(self.window)
             #sdl2.ext.quit()
@@ -40,13 +50,16 @@ class Gui():
         self.root_needle_angle = self.tac_needle.angle
 
     def refresh_gui(self):
-        self.spriterenderer.process(self.world, [self.ipc, self.tac_needle, self.speed_needle])
+        self.spriterenderer.process(self.world, [self.ipc, self.tac_needle, self.speed_needle, self.steering_wheel])
 
     def rotate_tac_needle(self, angle):
         self.tac_needle.angle = self.root_needle_angle + angle
     
     def rotate_speed_needle(self, angle):
         self.speed_needle.angle = self.root_needle_angle + angle
+
+    def rotate_steering_wheel(self, angle):
+        self.steer_wheel.angle = self.steering_wheel.angle + angle
 
 class needleEntity(sdl2.ext.Entity):
     def __init__(self, world, sprite, angle):
