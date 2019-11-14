5f52fb1c0b2a3fc24217766c9bd4780a7c8b1d07
Matt Appel <appel.60@osu.edu>
2019-11-14 12:33:15 -0500

    updated structure

--- setup/setupvcan.sh
+++ setup/setupvcan.sh
@@ -3,4 +3,4 @@
 sudo modprobe vcan
 sudo ip link add dev vcan0 type vcan
 sudo ip link set up vcan0
-pip3 intall Pillow cantools numpy pygame matplotlib
+pip3 install Pillow cantools numpy pygame matplotlib
